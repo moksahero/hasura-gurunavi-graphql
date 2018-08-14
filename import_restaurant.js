88e33e8ccd264c868974e39ef9cb583e613331b7
Takeshi Amano <moksahero@gmail.com>
2018-08-14 11:40:28 +0900

    renamed to import_restaurant.js

--- import_restaurant.js
+++ import_restaurant.js
@@ -15,42 +15,12 @@ axios.get('https://api.gnavi.co.jp/RestSearchAPI/20150630/', {
     areacode_l: 'AREAL2101'
   }
 }).then(result => {
-  result.data.rest.forEach(async value => {
-    /*
-    const restaurantDetails = {
-      gurunaviId: value.id,
-      update_date: value.update_date,
-      name: value.name,
-      name_kana: value.name_kana,
-      latitude: value.latitude,
-      longitude: value.longitude,
-      category: value.category,
-      url: value.url,
-      url_mobile: value.url_mobile,
-      shop_image1: value.shop_image1,
-      shop_image2: value.shop_image2,
-      qrcode: value.qrcode,
-      address: value.address,
-      tel: value.tel,
-      tel_sub: value.tel_sub,
-      fax: value.fax,
-      opentime: value.opentime,
-      holiday: value.holiday,
-      parking_lots: value.parking_lots,
-      budget: value.budget,
-      party: value.party,
-      lunch: value.lunch,
-      credit_card: value.credit_card,
-      e_money: value.e_money
-    }
-    */
-
-    const createRestaurant = `mutation insert_restaurant (
-      $gurunavi_id: String!,
-      $name:  String!,
-      $name_kana: String!,
-      $longitude: Float!,
-      $latitude: Float!) {
+  const createRestaurant = `mutation insert_restaurant (
+    $gurunavi_id: String!,
+    $name:  String!,
+    $name_kana: String!,
+    $longitude: Float!,
+    $latitude: Float!) {
       insert_restaurant(
         objects: [
           {
@@ -69,7 +39,7 @@ axios.get('https://api.gnavi.co.jp/RestSearchAPI/20150630/', {
         }
       }
     }`
-
+    
     const restaurantDetails = {
       gurunavi_id: value.id,
       name: value.name,
